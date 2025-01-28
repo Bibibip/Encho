d25db97fd2f79513d5f6ca9436d8101baf9bf189
Encho <sunbi2003@gmail.com>
2025-01-28 11:20:03 +0900

    14주차 커밋

---
+++ quiz.js
@@ -0,0 +1,12 @@
+//1번
+//state.subscribe(listener)
+
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1}),
+        [DECREASE]: (state, action) => ({ number: state.number - 1}),
+    },
+    initialState,
+);
+
+export default counter;
